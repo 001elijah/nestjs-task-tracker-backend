4b96f71e1f4c9cd85dd88ebe87aba783144f3faa
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-08-05 11:04:22 +0300

    ADD: create-task.dto.ts, update-task.dto.ts; UPDATE: tasks.controller.ts

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  @Get() getTasks() {
+    return [];
+  }
+
+  @Get(':id') getOneTask(@Param('id') id: string) {
+    console.log('get');
+    return { id };
+  }
+
+  @Post() createTask(@Body() createTaskDto: CreateTaskDto) {
+    return {
+      id: createTaskDto.id,
+      text: createTaskDto.text,
+      day: createTaskDto.day,
+      reminder: createTaskDto.reminder,
+    };
+  }
+
+  @Put(':id') updateTask(
+    @Param('id') id: string,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ) {
+    return {
+      id,
+      text: updateTaskDto.text,
+      day: updateTaskDto.day,
+      reminder: updateTaskDto.reminder,
+    };
+  }
+  @Delete(':id') deleteTask(@Param('id') id: string) {
+    return { id };
+  }
+}
